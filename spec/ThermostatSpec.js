24c5145639aadbb903caa0289467675e5e058660
Rodney Cullen <rodcul@gmail.com>
2015-05-28 16:58:14 +0100

    Power saving mode on by default

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,19 +1,42 @@
-describe("Thermostat", function(){
-    it("starts at 20 degrees",function(){
-      var thermostat = new Thermostat;
-      expect(thermostat.temperature).toEqual(20);
-    });
+describe("Thermostat", function() {
+	it("starts at 20 degrees", function() {
+		var thermostat = new Thermostat;
+		expect(thermostat.temperature).toEqual(20);
+	});
 
-    it("can increase the temp by 1",function(){
-      var thermostat = new Thermostat;
-      thermostat.increase();
-      expect(thermostat.temperature).toEqual(21);
-    });
+	it("can increase the temp by 1", function() {
+		var thermostat = new Thermostat;
+		thermostat.increase();
+		expect(thermostat.temperature).toEqual(21);
+	});
 
-    it("maximum temperature is 32 degrees",function(){
-      var thermostat = new Thermostat;
-      thermostat.temperature = 31;
-      thermostat.increase();
-      expect(function(){thermostat.increase()}).toThrow();
-    });
+	it("can decrease the temp by 1", function() {
+		var thermostat = new Thermostat;
+		thermostat.decrease();
+		expect(thermostat.temperature).toEqual(19);
+	});
+
+	it("maximum temperature is 32 degrees", function() {
+		var thermostat = new Thermostat;
+		thermostat.temperature = 31;
+		thermostat.increase();
+		expect(function() {
+			thermostat.increase()
+		}).toThrow();
+	});
+
+	it("minimum temperature is 10 degrees", function() {
+		var thermostat = new Thermostat;
+		thermostat.temperature = 11;
+		thermostat.decrease();
+		expect(function() {
+			thermostat.decrease()
+		}).toThrow();
+	});
+
+  it("power saving mode on by default", function(){
+    var thermostat = new Thermostat;
+    expect(thermostat.powerSavingMode).toEqual(true);
+  });
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,10 +1,18 @@
 function Thermostat() {
   this.temperature = 20;
+  this.powerSavingMode = true
 };
 
 Thermostat.prototype.increase = function() {
   if (this.temperature == 32) {
-    throw "JS sucks";
+    throw "Maxiumum temperature is 32 degrees";
   }
   this.temperature += 1;
+};
+
+Thermostat.prototype.decrease = function () {
+  if (this.temperature == 10) {
+    throw "Minimum temperature is 10 degrees";
+  }
+  this.temperature -= 1;
 };
