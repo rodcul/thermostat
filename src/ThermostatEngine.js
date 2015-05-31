b168d222b0726455237c4d76150565ad77fe7d08
Rodney Cullen <rodcul@gmail.com>
2015-05-31 13:29:20 +0100

    Added power saving mode to frontend

--- src/ThermostatEngine.js
+++ src/ThermostatEngine.js
@@ -1,42 +1,40 @@
 function Thermostat() {
-  this.temperature = 20;
-  this.powerSavingMode = true
+	this.temperature = 20;
+	this.powerSavingMode = true
 };
 
 Thermostat.prototype.increase = function() {
-  if (this.temperature == 32 && this.powerSavingMode == false) {
-    throw "Maxiumum temperature is 32 degrees with power saving mode off";
-  }
+	if (this.temperature == 32 && this.powerSavingMode == false) {
+		throw "Maxiumum temperature is 32 degrees with power saving mode off";
+	} else if (this.temperature == 25 && this.powerSavingMode == true) {
+		throw "Maxiumum temperature is 25 degrees with power saving mode on";
+	}
 
-  else if (this.temperature == 25 && this.powerSavingMode == true) {
-    throw "Maxiumum temperature is 25 degrees with power saving mode on";
-  }
-
-  this.temperature += 1;
+	this.temperature += 1;
 };
 
-Thermostat.prototype.decrease = function () {
-  if (this.temperature == 10) {
-    throw "Minimum temperature is 10 degrees";
-  }
-  this.temperature -= 1;
+Thermostat.prototype.decrease = function() {
+	if (this.temperature == 10) {
+		throw "Minimum temperature is 10 degrees";
+	}
+	this.temperature -= 1;
 };
 
 Thermostat.prototype.reset = function() {
-  this.temperature = 20;
+	this.temperature = 20;
 };
 
-Thermostat.prototype.color = function () {
-  if (this.temperature <= 18) {
-    return "green";
-  } else if (this.temperature <= 25) {
-    return "yellow";
-  } else {
-    return "red";
-  }
+Thermostat.prototype.color = function() {
+	if (this.temperature <= 18) {
+		return "green";
+	} else if (this.temperature <= 25) {
+		return "yellow";
+	} else {
+		return "red";
+	}
 
 };
 
 Thermostat.prototype.togglePowerSavingMode = function() {
-  this.powerSavingMode = !this.powerSavingMode
+	this.powerSavingMode = !this.powerSavingMode
 };
--- src/ThermostatWeb.js
+++ src/ThermostatWeb.js
@@ -1,19 +1,29 @@
 var thermostat = new Thermostat();
 
-$(document).ready( function() {
-  $('#up').on('click', function(){
-    thermostat.increase();
-    $('#temperature').html(thermostat.temperature);
+$(document).ready(function() {
+	$('#up').on('click', function() {
+		thermostat.increase();
+		$('#temperature').html(thermostat.temperature);
 
-  });
+	});
 
-  $('#down').on('click', function(){
-    thermostat.decrease();
-    $('#temperature').html(thermostat.temperature);
-  });
+	$('#down').on('click', function() {
+		thermostat.decrease();
+		$('#temperature').html(thermostat.temperature);
+	});
 
-  $('#reset').on('click', function(){
-    thermostat.reset();
-    $('#temperature').html(thermostat.temperature);
-  });
+	$('#reset').on('click', function() {
+		thermostat.reset();
+		$('#temperature').html(thermostat.temperature);
+	});
+
+	$('#togglePowerSavingMode').on('click', function() {
+		thermostat.togglePowerSavingMode();
+		if (thermostat.powerSavingMode) {
+			$('#powerSavingMode').html('ON')
+		} else {
+			$('#powerSavingMode').html('OFF')
+		}
+
+	});
 });
